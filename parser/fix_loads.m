5c6b22735e422733514eb3a552f044360098914f
Igor Borges <devatdevian@gmail.com>
2020-10-20 18:05:50 -0300

    Correção de forças entre carregamentos

--- parser/fix_loads.m
+++ parser/fix_loads.m
@@ -1,6 +1,7 @@
 function out = fix_loads() 
     
     global carregamentos;
+    global forcas_verticais;
     global apoios;
 
     for i = 1:length(apoios)
@@ -16,6 +17,21 @@ function out = fix_loads()
         end
 
     end
+
+    for i = 1:length(forcas_verticais)
+        force_x = forcas_verticais{i}{2};
+        for j = 1:length(carregamentos)
+            load_xi = carregamentos{j}{1};
+            load_xf = carregamentos{j}{2};
+            if ((load_xi < force_x) && (force_x < load_xf))
+                carregamentos{end+1} = carregamentos{j};
+                carregamentos{j}{2} = force_x;
+                carregamentos{end}{1} = force_x;
+            end
+        end
+
+    end
+
 
 endfunction
 
