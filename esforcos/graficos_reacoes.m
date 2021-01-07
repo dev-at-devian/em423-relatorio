53c4077a44734634fa4d78f8b5bc09f460a4087f
Rafael Sartori M. Santos <rafaelsartori96@users.noreply.github.com>
2021-01-07 09:34:08 -0300

    Corrigindo uso de variáveis e algumas equações p/ tensões

--- esforcos/graficos_reacoes.m
+++ esforcos/graficos_reacoes.m
@@ -60,14 +60,7 @@ function graficos_reacoes(viga, apoios, singfun_carregamentos, singfun_forcas_x,
             defineconst(torcao, apoios{i}.position, 0);
         endif
     endfor
-    
-    # Ponto A (mais alto no y e no meio do z)
-    tensao_normal_A = integrate_noconst(forcas_x) * (1 / viga.area) - integrate_noconst(momentum) * viga.radius * (1 / viga.I);
-    tensao_cisalhamento_A = integrate_noconst(torques) * viga.radius * (1 / viga.Ip)
-    
-    # Ponto B (no meio em y e mais a direita em z)
-    tensao_normal_B = integrate_noconst(forcas_x) * (1 / viga.area);
-    tensao_cisalhamento_B = integrate_noconst(torques) * viga.radius * (1 / viga.Ip) + (4/3) * integrate_noconst(forcas_y) * (1/viga.area) * correction_factor;
+
 
     figure(1);
     plot(normal, [0, viga.width], "linewidth", 2, "color", [1, 0.435, 0]);
@@ -132,14 +125,77 @@ function graficos_reacoes(viga, apoios, singfun_carregamentos, singfun_forcas_x,
     title("Deflexão da viga");
     xlabel("x [m]");
     ylabel("v(x) [m]");
-    
-    figure(9);
-    plot(tensao_normal_A, [0, viga.width], "linewidth", 2, "color", [0.192, 0.106, 0.573]);
-    grid on;
-    set(gca, "fontsize", 12);
-    title("Tensao normal no ponto  A");
-    xlabel("x [m]");
-    ylabel("T(x) [Nm]");
+
+
+    # Quando não temos uma barra (quando temos uma viga circular ou circular vazada), devemos
+    # calcular tensões, tensões principais e de cisalhamento máximas em pontos extremos
+    if !strcmp(viga.type, "bar")
+        raio = viga.radius;
+        if strcmp(viga.type, "hollow")
+            # Se temos um cilindro vazado, temos que manualmente pegar o raio externo
+            raio = viga.outer_radius;
+        endif
+
+        # Ponto A
+        # (Maior valor no eixo y, centro do eixo z)
+        # y, z = (viga.raio, 0)
+
+        # Calculamos tensão normal provocada por força normal
+        #   sigma = forças normais N * (1 / área)
+        tensao_normal_normal_A = normal * (1 / viga.area);
+
+        # Calculamos tensão normal provocada por momento
+        #   sigma = - momento M * posição no eixo y * (1 / momento de inércia no eixo y)
+        # (momento de inércia no eixo y tirado dos exercícios da aula 9 -- estava na dúvida se usávamos I polar)
+        tensao_normal_momento_A = - momentum * raio * (1 / viga.Iy);
+
+        # Calculamos a tensão normal resultante
+        #   sigma = sigma_normal + sigma_momento
+        tensao_normal_A = tensao_normal_normal_A + tensao_normal_momento_A;
+
+        # Calculamos a tensão de cisalhamento provocada por forças cortantes
+        #   tau = (4 / 3) * (forças cortantes V) * (1 / área) * fator de correção
+        # onde
+        #   Para circular maciça:
+        #       fator de correção = 1
+        #   Para circular vazada:
+        #       fator de correção = (raio externo ^ 2 + raio externo * raio interno + raio interno ^ 2) / (raio externo ^ 2 + raio interno ^ 2)
+        fator_correcao = 1;
+        if strcmp(viga.type, "hollow")
+            fator_correcao = (viga.outer_radius^2 + viga.outer_radius * viga.inner_radius + viga.inner_radius^2) / (viga.outer_radius^2 + viga.inner_radius^2);
+        endif
+        tensao_cisalhamento_cortantes_A = (4 / 3) * forca_cortante * (1 / viga.area) * fator_correcao;
+
+        # Calculamos a tensão de cisalhamento provocada por torção
+        #   tau = torques internos T * posição no raio * (1 / momento de inércia polar)
+        tensao_cisalhamento_torcao_A = torque_interno * raio * (1 / viga.Ip);
+
+        # Calculamos a tensão de cisalhamento provocada por torção
+        #   tau = tau_cortantes + tau_torcao
+        tensao_cisalhamento_A = tensao_cisalhamento_cortantes_A + tensao_cisalhamento_torcao_A;
+
+        # Ponto B
+        # (Centro do eixo y e maior valor dentro da viga no eixo z)
+        # y, z = (viga.raio, 0)
+
+
+        figure(9);
+        plot(tensao_normal_A, [0, viga.width], "linewidth", 2, "color", [0.192, 0.106, 0.573]);
+        grid on;
+        set(gca, "fontsize", 12);
+        title("Tensao normal no ponto  A");
+        xlabel("x [m]");
+        ylabel("sigma(x) [Pa]");
+
+        figure(10);
+        plot(tensao_cisalhamento_A, [0, viga.width], "linewidth", 2, "color", [0.192, 0.106, 0.573]);
+        grid on;
+        set(gca, "fontsize", 12);
+        title("Tensao de cisalhamento no ponto  A");
+        xlabel("x [m]");
+        ylabel("tau(x) [Pa]");
+
+    endif
 
 endfunction
 
